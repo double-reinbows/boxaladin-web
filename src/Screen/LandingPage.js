366839b89521660fd56e06f52d5db08ccef94f80
tezaharsony <teza.harsony230394@gmail.com>
2018-01-14 23:31:11 +0700

    clear the web and make flex

--- src/Screen/LandingPage.js
+++ src/Screen/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import logo from '../logo.jpeg'
 import promotion from '../promotion.jpg'
+import phone from '../phone.jpeg'
 
 import { loginAction } from '../actions/'
 
@@ -12,15 +13,16 @@ class LandingPage extends Component {
         <div style= {{ height: '300px', backgroundColor: 'black', width: '100%'}}>
            <img  src={logo} width="350"  height="300" alt='logo'/>
         </div>
-        <div style= {{ height: '80px', backgroundColor: 'green', width: '100%'}} >
-          <div >
-          <a>Home</a>
+        <div style= {{ height: '80px', backgroundColor: 'green', width: '70%', display: 'flex' , alignItems: 'center', justifyContent: 'space-around', fontSize: '50px'}} >
+          <a >Home</a>
           <a>How its works</a>
           <a>Contact</a>
-          </div>
         </div>
         <div style= {{ height: '500px', backgroundColor: 'black', width: '100%'}}>
            <img  src={promotion} width="800" height="500" alt='logo'/>
+        </div>
+        <div>
+        <img  src={phone} width="350"  height="400" alt='logo' style={{ position: 'absolute', top: '0', right: '100px'}}/>
         </div>
       </div>
     )
