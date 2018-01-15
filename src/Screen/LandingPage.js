b7c19a568dfec47df454b8c85f30346e83466c84
Thoriqnf <Thoriqnfaizal@gmail.com>
2018-01-15 16:33:32 +0700

    fixing responsive thoriq

--- src/Screen/LandingPage.js
+++ src/Screen/LandingPage.js
@@ -10,10 +10,10 @@ class LandingPage extends Component {
   render () {
     return (
       <div>
-        <div style= {{ height: '300px', backgroundColor: 'black', width: '100%'}}>
+        <div style= {{ height: '400px', backgroundColor: 'black', width: '100%'}}>
           <ul >
-            <li><img  src={logo} width="380"  height="330" alt='logo' class='img-responsive'/></li>
-            <li><h1 style= {{ color: 'white', fontWeight: 'bold', position: 'absolute', left: '550px', top: '250px' }}> The more you see it, the cheaper it gets</h1></li>
+            <img  src={logo} width="380"  height="330" alt='logo' class='img-responsive'/>
+            <h1 style= {{ color: 'white', fontWeight: 'bold', position: 'absolute', left: '550px', top: '250px' }}> The more you see it, the cheaper it gets</h1>
           </ul>
         </div>
         <nav className="navbar navbar-default" style= {{ height: '100px' ,paddingTop: '30px', backgroundColor: 'green', marginBottom: '0px', borderColor: 'transparent'}}>
@@ -28,19 +28,19 @@ class LandingPage extends Component {
           </div>
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-2" style = {{ float: 'left', position: 'relative'}}>
-          <ul className="nav navbar-nav navbar-right" style = {{ color: 'white'}}>
-            <li style = {{ fontSize: '30px'}}><p style = {{ color : 'white', paddingLeft: '130px', paddingRight: '150px'}} >Home</p></li>
-            <li style = {{ fontSize: '30px'}}><p style = {{ color : 'white', paddingLeft: '150px', paddingRight: '150px'}} >How it works</p></li>
-            <li style = {{ fontSize: '30px'}}><p style = {{ color : 'white', paddingLeft: '150px', paddingRight: '30px'}} >Contact Us</p></li>
-          </ul>
+            <ul className="nav navbar-nav navbar-right" style = {{ color: 'white'}}>
+              <li style = {{ fontSize: '30px'}}><p style = {{ color : 'white', paddingLeft: '130px', paddingRight: '150px'}} >Home</p></li>
+              <li style = {{ fontSize: '30px'}}><p style = {{ color : 'white', paddingLeft: '150px', paddingRight: '150px'}} >How it works</p></li>
+              <li style = {{ fontSize: '30px'}}><p style = {{ color : 'white', paddingLeft: '150px', paddingRight: '30px'}} >Contact Us</p></li>
+            </ul>
           </div>
         </div>
         </nav>
         <div style= {{ height: '480px', backgroundColor: 'black', width: '100%'}}>
            <img  src={promotion} alt='logo' class='img-responsive'  width="1000"/>
         </div>
-        <div>
-          <img  src={phone} width="350"  height="600" alt='logo' style={{ position: 'absolute', top: '300px', right: '100px'}}/>
+        <div className="collapse navbar-collapse">
+          <img  src={phone} width="450px"  height="700px" alt='logo'  style={{ position: 'absolute', top: '200px', right: '150px'}}/>
         </div>
       </div>
     )
