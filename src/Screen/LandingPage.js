b8e276680011dea4245c2cd42bf0126cd06bf5cf
Thoriqnf <Thoriqnfaizal@gmail.com>
2018-01-15 19:34:54 +0700

    built new responsive web

--- src/Screen/LandingPage.js
+++ src/Screen/LandingPage.js
@@ -10,38 +10,42 @@ class LandingPage extends Component {
   render () {
     return (
       <div>
-        <div style= {{ height: '400px', backgroundColor: 'black', width: '100%'}}>
-          <ul >
-            <img  src={logo} width="380"  height="330" alt='logo' class='img-responsive'/>
-            <h1 style= {{ color: 'white', fontWeight: 'bold', position: 'absolute', left: '550px', top: '250px' }}> The more you see it, the cheaper it gets</h1>
-          </ul>
-        </div>
-        <nav className="navbar navbar-default" style= {{ height: '100px' ,paddingTop: '30px', backgroundColor: 'green', marginBottom: '0px', borderColor: 'transparent'}}>
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-          </div>
 
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-2" style = {{ float: 'left', position: 'relative'}}>
-            <ul className="nav navbar-nav navbar-right" style = {{ color: 'white'}}>
-              <li style = {{ fontSize: '30px'}}><p style = {{ color : 'white', paddingLeft: '130px', paddingRight: '150px'}} >Home</p></li>
-              <li style = {{ fontSize: '30px'}}><p style = {{ color : 'white', paddingLeft: '150px', paddingRight: '150px'}} >How it works</p></li>
-              <li style = {{ fontSize: '30px'}}><p style = {{ color : 'white', paddingLeft: '150px', paddingRight: '30px'}} >Contact Us</p></li>
+        <div className="jumbotron jumbo">
+          <div className="row">
+            <div className="col-sm-2">
+              <img  src={logo} width="300"  height="300" alt='logo' className='img-responsive'/>
+            </div>
+            <div className="banner1 col-sm-8">
+              <h2>Semakin dilihat semakin murah</h2>
+            </div>
+            <div className="col-sm-4">
+            </div>
+          </div>
+        </div>
+
+        <nav class="navbar navbar-default navbar-collapse">
+          <div class="container-fluid">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">How it works</a></li>
+              <li><a href="#">Contact us</a></li>
             </ul>
           </div>
-        </div>
         </nav>
-        <div style= {{ height: '480px', backgroundColor: 'black', width: '1000%'}}>
-           <img  src={promotion} alt='logo' class='img-responsive'  width="1000"/>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4">
+              <img  src={promotion} alt='logo' class='img-responsive'  width="1000"/>
+            </div>
+          </div>
         </div>
+
         <div className="collapse navbar-collapse">
-          <img  src={phone} width="450px"  height="700px" alt='logo'  style={{ position: 'absolute', top: '200px', right: '150px'}}/>
+          <img  src={phone} width="450px"  height="700px" alt='logo'  style={{ position: 'absolute', top: '200px', right: '50px'}}/>
         </div>
+
       </div>
     )
   }
