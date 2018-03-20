b5ffe5a9a1b8ac22c9f99a82217398edbed811bd
Thoriqnf <Thoriqnfaizal@gmail.com>
2018-03-20 14:12:13 +0700

    fixing timer bidding

--- src/Screen/Bidding.js
+++ src/Screen/Bidding.js
@@ -16,8 +16,8 @@ class Bidding extends React.Component {
     super(props)
     this.state = {
       productUnlocked: {},
-			count: 9999,
-      initCount: 9999,
+			count: 15,
+      initCount: 15,
       isWatching: false
     }
 
