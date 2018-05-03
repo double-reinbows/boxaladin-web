c30fed5af704afa975771bb61b520d5f4484c8df
Rei <rei@kickavenue.com>
2018-05-03 19:32:04 +0700

    Changing component to unconnected class. Handling network error.

--- src/Screen/RequestResetPassword.js
+++ src/Screen/RequestResetPassword.js
@@ -1,42 +1,28 @@
+// @flow
 import React from 'react'
 import axios from 'axios'
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
 import { Button, Form, FormGroup, Input } from 'reactstrap'
 
-class RequestResetPassword extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: null,
-      notif: ''
-    }
+type State = {
+  email: string | null,
+  notif: string,
+  buttonText: string,
+};
+type Props = {
+  history: Array,
+};
+export default class RequestResetPassword<Props, State> extends React.Component {
+  state: State = {
+    email: null,
+    notif: '',
+    buttonText: 'Kirim email',
   }
 
-  render() {
-  
-
-    return (
-      <div className="RequestReset">
-        <h1 className="RequestReset__text">Request Reset Password</h1>
-
-        <Form onSubmit={ (e) => this.sendLink(e) }>
-          <FormGroup>
-             <Input onChange={(e) => this.setState({ email: e.target.value })} type="email" name="email" id="email" placeholder="email" />
-          </FormGroup>
-          <FormGroup>
-            <Button color="success" size="lg" block>send link</Button>
-          </FormGroup>
-        </Form>
-        <label className="alert__resetPassword">{this.state.notif}</label>
-      </div>
-    )
-  }
-
-  sendLink(e) {
-    e.preventDefault()
+  sendLink(e: SyntheticInputEvent<HTMLInputElement>): void {
+    e.preventDefault();
 
     if (this.state.email !== null) {
-
       axios({
         method: 'POST',
         url: `${process.env.REACT_APP_API_HOST}/forgotpassword`,
@@ -50,37 +36,56 @@ class RequestResetPassword extends React.Component {
       .then(({data}) => {
         if (data.msg === 'email sent') {
           this.setState({
-            notif: "Email Sent",
-          })
-          this.props.history.push('/')
+            notif: 'Email reset password ulang terkirim!',
+            buttonText: 'Terkirim!',
+          });
+          this.props.history.push('/');
         } else {
           console.log('data')
         }
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        if (err.message == 'Network Error') {
+          this.setState({
+            notif: 'Mohon chek koneksi internet Anda.',
+            buttonText: 'Kirim email',
+          });
+        } else {
+          this.setState({
+            notif: 'Mohon maaf ada masalah dengan sistem kami. Mohon coba ulang beberapa saat lagi',
+            buttonText: 'Kirim email',
+          })
+        }
+      });
+      this.setState({buttonText: 'Sending email....'})
 
     } else {
       this.setState({
-        notif: "Email Harus diisi",
+        notif: 'Email Harus diisi',
       })
     }
 
   }
 
-}
+  render() {
+    let {buttonText, notif} = this.state;
+    return (
+      <div className="RequestReset">
+        <h1 className="RequestReset__text">Request Reset Password</h1>
 
-const mapStateToProps = (state) => {
-  return {
-    //
+        <Form onSubmit={ (e: SyntheticInputEvent<HTMLInputElement>) => this.sendLink(e) }>
+          <FormGroup>
+             <Input onChange={(e: SyntheticInputEvent<HTMLInputElement>) => this.setState({email: e.target.value}) } type="email" name="email" id="email" placeholder="email" />
+          </FormGroup>
+          <FormGroup>
+            <Button color="success" size="lg" block>{buttonText}</Button>
+          </FormGroup>
+        </Form>
+        <label className="alert__resetPassword">{notif}</label>
+      </div>
+    )
   }
+
+
+
 }
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    //
-  }
-}
-
-const connectComponent = connect(mapStateToProps, mapDispatchToProps)(RequestResetPassword)
-
-export default connectComponent
