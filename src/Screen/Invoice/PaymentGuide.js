87843cc13ca9025a53dc8c1d5f17cb29fe408d7c
tezaharsony <teza.harsony230394@gmail.com>
2018-05-30 14:46:07 +0700

    add payment retail

--- src/Screen/Invoice/PaymentGuide.js
+++ src/Screen/Invoice/PaymentGuide.js
@@ -308,7 +308,7 @@ const Guide = (props) => {
             Setelah penerima pembayaran di temukan oleh sistem alfamart, staff kasih akan menanyakan code pembayaran yang harus di input ke sistem alfamart.
           </li>
           <li>
-            Kode pembayaran tersebut adalah "Boaladin"
+            Kode pembayaran tersebut adalah {props.invoice.payment.availableRetail}
           </li>
           <li>
             Detail pembayaran akan keluar di sistem alfamart dan pembayaran sudah bisa dilakukan
@@ -319,5 +319,6 @@ const Guide = (props) => {
     </TabContent>
   )
 }
+
 
 export default Guide
