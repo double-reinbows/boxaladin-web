598b9463b2212d09df18f1a7972b82f2f11f9b45
andrew <sbstn.andrew56@gmail.com>
2018-09-17 12:06:45 +0700

    fix bug carousel

--- src/Screen/Mobile/Home/Top/Carousel.js
+++ src/Screen/Mobile/Home/Top/Carousel.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-import { UncontrolledCarousel } from 'reactstrap';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 import HelperAxios from '../../../../utils/axios'
 
 class CarouselMobile extends Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = { 
+      activeIndex: 0,
       items: []
-    }
+    };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
   }
-  
+
   componentDidMount() {
     this.getPictures()
   }
@@ -25,10 +37,63 @@ class CarouselMobile extends Component {
 
   }
 
-  render() { 
-    return (  
-      <UncontrolledCarousel items={this.state.items} />
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    const {items} = this.state
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    const {items} = this.state
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex, items } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
+        >
+          <img className="d-block img-fluid" src={item.src} alt={item.altText} />
+          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+      </Carousel>
     );
   }
 }
+
+
 export default CarouselMobile;
