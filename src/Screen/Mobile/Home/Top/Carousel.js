89a89fc27c14299fab2cc93dfe262c89fb566d45
andrew <sbstn.andrew56@gmail.com>
2018-10-11 14:25:40 +0700

    add new npm for carousel

--- src/Screen/Mobile/Home/Top/Carousel.js
+++ src/Screen/Mobile/Home/Top/Carousel.js
@@ -1,26 +1,14 @@
-import React, { Component } from 'react';
-import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption
-} from 'reactstrap';
+import React, { Component } from "react";
+import Slider from "react-slick";
 import { Link } from 'react-router-dom';
 import HelperAxios from '../../../../utils/axios'
 
-class CarouselMobile extends Component {
+export default class CarouselMobile extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      activeIndex: 0,
       items: []
     };
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-    this.goToIndex = this.goToIndex.bind(this);
-    this.onExiting = this.onExiting.bind(this);
-    this.onExited = this.onExited.bind(this);
   }
 
   componentDidMount() {
@@ -35,68 +23,39 @@ class CarouselMobile extends Component {
       })
     })
     .catch(err => console.log(err))
-
   }
 
-  onExiting() {
-    this.animating = true;
-  }
-
-  onExited() {
-    this.animating = false;
-  }
-
-  next() {
-    const {items} = this.state
-    if (this.animating) return;
-    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
-    this.setState({ activeIndex: nextIndex });
-  }
-
-  previous() {
-    const {items} = this.state
-    if (this.animating) return;
-    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
-    this.setState({ activeIndex: nextIndex });
-  }
-
-  goToIndex(newIndex) {
-    if (this.animating) return;
-    this.setState({ activeIndex: newIndex });
-  }
+    renderImage = () => {
+      const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        lazyLoad: true,
+      };
+      return(
+        <div>
+          <Slider {...settings}>
+            {
+            this.state.items.map((item, index) => {
+              return (
+              <Link to={item.caption}>
+                <img className="d-block img-fluid" src={item.src} alt={item.altText} />
+              </Link>
+                )
+            })
+            }
+          </Slider>
+        </div>
+      )
+    }
 
   render() {
-    const { activeIndex, items } = this.state;
-
-    const slides = items.map((item) => {
-      return (
-        <CarouselItem
-          onExiting={this.onExiting}
-          onExited={this.onExited}
-          key={item.src}
-        >
-        <Link to={item.caption}>
-          <img className="d-block img-fluid" src={item.src} alt={item.altText} />
-        </Link>
-          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
-        </CarouselItem>
-      );
-    });
-
     return (
-      <Carousel
-        activeIndex={activeIndex}
-        next={this.next}
-        previous={this.previous}
-      >
-        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-        {slides}
-        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
-      </Carousel>
+      <div>
+        {this.renderImage()}
+      </div>
     );
   }
 }
-
-
-export default CarouselMobile;
