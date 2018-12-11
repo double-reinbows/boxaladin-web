e8561315ad4b29b2c3168f83e395f0ae7e1c7247
Afdhalul Ihsan Rosyidi <ihsan.r28@gmail.com>
2018-12-11 17:22:06 +0700

    add script to footer

--- src/Screen/Components/Footer/FooterBot.js
+++ src/Screen/Components/Footer/FooterBot.js
@@ -5,6 +5,7 @@ type Props = {};
 export default  function FooterBot(props: Props) {
   return (
     <div className="footer">
+      <script id="domain" data-name="1204653" type="text/javascript" src="http://analytics.boxaladin.asia/js/analytics_js/client.js"></script>
       <label className="footer__label">Dilindungi oleh Hak Cipta dan berbadan hukum resmi.</label>
       <div className="footer__labelBot">
         <label className="footer__label">Est.2016 PT BOX ALADIN ASIAPASIFIC</label>
